4284285b9a661294d1d85fc94c9b3668f5d89802
Jang Sungil <jasuil@daum.net>
2025-10-10 17:31:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const app = express();
 app.use(express.static('public'));
 
 app.use('/video', express.static('video'));
-/*
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(`${__dirname}/public/player.html`);
+
+m3u8List = [
+	'/video/livelist.m3u8',
+	'/video/livelist2.m3u8',
+	'/video/livelist3.m3u8',
+	'/video/livelist4.m3u8',
+]
+
+app.get('/get-m3u8', (req, res) => {
+	if (m3u8List.length > 0) {
+		return res.status(200).sendFile(__dirname + m3u8List.shift());
+	} else {
+		return res.status(204).send('No Content');
+	}
+  // return res.status(200).sendFile(`${__dirname}/public/player.html`);
 });
-*/
+
 
 const server = app.listen(8888);
